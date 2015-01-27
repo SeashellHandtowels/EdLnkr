c971554ff11fea1cf93f6299cdfb0625574504a1
Monsterblader <Monsterblader@gmail.com>
2015-01-27 15:12:26 -0800

    Create skeleton of user plans view page.  Begin create plan page.

--- client/app/space/space.controller.js
+++ client/app/space/space.controller.js
@@ -1,6 +1,23 @@
 'use strict';
 
 angular.module('edLnkrApp')
-  .controller('SpaceCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('SpaceCtrl', function ($scope, $http) {
+    $scope.message = 'This is the space view.';
+    $scope.plans = [];
+
+    $http.get('/api/plans').success(function(plans) {
+      $scope.plans = plans;
+    });
+
+    $scope.addPlan = function() {
+      // if($scope.newThing === '') {
+      //   return;
+      // }
+      // $http.post('/api/plans', { name: $scope.newThing });
+      // $scope.newThing = '';
+    };
+
+    $scope.removePlan = function(plan) {
+      $http.delete('/api/plans/' + plan._id);
+    };
   });
