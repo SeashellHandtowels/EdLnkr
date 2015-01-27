85adb3b6867fa5d286f15a22fcf717d68d9ef295
Monsterblader <Monsterblader@gmail.com>
2015-01-27 15:12:26 -0800

    Create skeleton of user plans view page.  Begin create plan page.

--- client/app/space/space.js
+++ client/app/space/space.js
@@ -8,5 +8,34 @@ angular.module('edLnkrApp')
         templateUrl: 'app/space/space.html',
         controller: 'SpaceCtrl',
         authenticate: true
-      });
+      })
+    .state('create', {
+      url: '/space/create',
+      templateUrl: 'app/space/space_create.html',
+      controller: 'SpaceCtrl',
+      authenticate: true
+    });
+
   });
+
+
+  // angular.module('edLnkrApp')
+  // .config(function ($stateProvider) {
+  //   $stateProvider
+  //     .state('login', {
+  //       url: '/login',
+  //       templateUrl: 'app/account/login/login.html',
+  //       controller: 'LoginCtrl'
+  //     })
+  //     .state('signup', {
+  //       url: '/signup',
+  //       templateUrl: 'app/account/signup/signup.html',
+  //       controller: 'SignupCtrl'
+  //     })
+  //     .state('settings', {
+  //       url: '/settings',
+  //       templateUrl: 'app/account/settings/settings.html',
+  //       controller: 'SettingsCtrl',
+  //       authenticate: true
+  //     });
+  // });
