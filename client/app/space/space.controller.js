caebb23bfbeb096968a991645c5571a401d5ad93
Monsterblader <Monsterblader@gmail.com>
2015-01-28 11:45:49 -0800

    Fix waffle issue #8 refactor addPlan to deal with link input not yet added to queue

--- client/app/space/space.controller.js
+++ client/app/space/space.controller.js
@@ -1,23 +1,24 @@
 'use strict';
 
 angular.module('edLnkrApp')
-  .controller('SpaceCtrl', function ($scope, $http, $rootScope) {
+  .controller('SpaceCtrl', function ($scope, $http, $rootScope, $location) {
     $scope.message = 'This is the space view.';
     $scope.plans = [];
     $scope.link = {};
-
+    $scope.planLinks = [];
     $http.get('/api/plans').success(function(plans) {
       $scope.plans = plans;
     });
 
     $scope.addAnotherLink = function(){
-      $scope.planLinks = $scope.planLinks || [];
-      var link = {
-        url: $scope.link.url,
-        description: $scope.link.description
-      };
-      $scope.planLinks.push(link);
-      $scope.link = {};
+      if ($scope.link.url){
+        var link = {
+          url: $scope.link.url,
+          description: $scope.link.description
+        };
+        $scope.planLinks.push(link);
+        $scope.link = {};
+      }
     };
 
     $scope.addPlan = function() {
@@ -25,6 +26,9 @@ angular.module('edLnkrApp')
       if($scope.title === undefined) {
         return;
       }
+
+      $scope.addAnotherLink();
+      
       var plan = {
         title: $scope.title,
         user: $rootScope.user._id,
@@ -35,6 +39,7 @@ angular.module('edLnkrApp')
       $http.post('/api/plans', plan)
       .success(function(data) {
         console.log('Plan created successfully: ' + data);
+        $location.path('/space');
       })
       .error(function(data) {
         console.log('There was an error when creating the plan: ' + data);
