a3cb2c1c05c6428fdbddf45419fc2186ef600cb5
James Kolonusz <jamesak@gmail.com>
2015-01-28 18:38:08 -0800

    Add ability to fetch a specific plan id from url in $state params property.

--- client/app/space/showplan/showplan.js
+++ client/app/space/showplan/showplan.js
@@ -4,7 +4,7 @@ angular.module('edLnkrApp')
   .config(function ($stateProvider) {
     $stateProvider
       .state('showplan', {
-        url: '/showplan',
+        url: '/showplan/:planId',
         templateUrl: 'app/space/showplan/showplan.html',
         controller: 'ShowplanCtrl'
       });
