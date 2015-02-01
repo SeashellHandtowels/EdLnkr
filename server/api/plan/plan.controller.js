df3ea0c9be2bd837d87d47ea30e701e6a3866dd8
Monsterblader <Monsterblader@gmail.com>
2015-02-01 14:39:36 -0800

    Fix reverted code.

--- server/api/plan/plan.controller.js
+++ server/api/plan/plan.controller.js
@@ -47,30 +47,25 @@ exports.create = function(req, res) {
 
 // Updates an existing plan in the DB.
 exports.update = function(req, res) {
-  if (Array.isArray(req.body)) {
-    var query = {_id: req.params.id};
-    Plan.update(query, {links: req.body});
-  } else {
-    if(req.body._id) { delete req.body._id; }
-    Plan.findById(req.params.id, function (err, plan) {
+  if(req.body._id) { delete req.body._id; }
+  Plan.findById(req.params.id, function (err, plan) {
+    if (err) {
+      return handleError(res, err);
+    }
+    if(!plan) {
+      return res.send(404);
+    }
+    var updated = _.merge(plan, req.body);
+    if (req.body.links) {
+      updated._doc.links = req.body.links;
+    }
+    updated.save(function (err) {
       if (err) {
         return handleError(res, err);
       }
-      if(!plan) {
-        return res.send(404);
-      }
-      //var temp = req.query.data.split('[')[1];
-      var tempLinks = req.body;
-      var updated = _.merge(plan, req.body);
-      updated._doc.links = tempLinks;
-      updated.save(function (err) {
-        if (err) {
-          return handleError(res, err);
-        }
-        return res.json(200, plan);
-      });
+      return res.json(200, plan);
     });
-  }
+  });
 };
 
 // Deletes a plan from the DB.
