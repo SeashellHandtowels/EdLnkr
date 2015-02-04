4ee1866137f2bda8d5cc3a0264a86b06acb2c2dd
Jared Noble <jsnoble1@gmail.com>
2015-02-04 11:06:38 -0700

    added search bar, working on home page

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -9,3 +9,17 @@ angular.module('edLnkrApp')
         controller: 'MainCtrl'
       });
   });
+angular.module('ui.bootstrap.carousel').controller('CarouselDemoCtrl', function ($scope) {
+  $scope.myInterval = 10000;
+  var slides = $scope.slides = [];
+  $scope.addSlide = function() {
+    var newWidth = 800 + slides.length + 1;
+    slides.push({
+      image: '../../assets/images/books.jpg' //+ newWidth
+      //text: ['Our app is cool!','Tons of features','Gorgeous Interface','Teach stuff'][slides.length % 4]
+    });
+  };
+  for (var i=0; i<1; i++) {
+    $scope.addSlide();
+  }
+});
