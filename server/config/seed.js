a48acc06b0f999945ee3ab803f15e879609fe4b5
minocys <harich.leung@gmail.com>
2015-02-04 22:50:21 -0500

    seed populates test db properly

--- server/config/seed.js
+++ server/config/seed.js
@@ -6,6 +6,7 @@
 'use strict';
 
 var User = require('../api/user/user.model');
+var Plan = require('../api/plan/plan.model');
 
 User.find({}).remove(function() {
   User.create({
@@ -13,93 +14,114 @@ User.find({}).remove(function() {
     name: 'Test User',
     email: 'test@test.com',
     password: 'test'
-  }, {
+  }, 
+  {
     provider: 'local',
     role: 'admin',
     name: 'Admin',
     email: 'admin@admin.com',
     password: 'admin'
-  }, function() {
+  }, function(err, user) {
       console.log('finished populating users');
+      
+      var plans = [];
+
+      plans.push(
+        {
+          date: new Date(),
+          title: 'Learn Javascript',
+          synopsis: 'join the world of programming',
+          user: user.id,
+          rating: '4',
+          category: 'Math'
+        }, 
+        {
+          date: new Date(),
+          title: 'Learn To Fly',
+          synopsis: 'flying is such an amazing expernce.',
+          user: user.id,
+          rating: '5',
+          category: 'other'
+        },
+        {
+          date: new Date(),
+          title: 'Carl Seigan rules',
+          synopsis: 'Need I say more?',
+          user: user.id,
+          rating: '5',
+          category: 'The Universe'
+        },
+        {
+          date: new Date(),
+          title: 'Learn to Cook',
+          synopsis: 'Be the life of the party by cooking.',
+          user: user.id,
+          rating: '3',
+          category: 'Food'
+        },
+        {
+          date: new Date(),
+          title: 'Klingon',
+          synopsis: 'nerds',
+          user: user.id,
+          rating: '2',
+          category: 'Languages'
+        },
+        {
+          date: new Date(),
+          title: 'Much Music Making',
+          synopsis: 'More bass in this place',
+          user: user.id,
+          rating: '4',
+          category: 'Music'
+        },
+        {
+          date: new Date(),
+          title: 'Mathematics by Socrates',
+          synopsis: 'because 1 + 1 =  wisdom',
+          user: user.id,
+          rating: '1',
+          category: 'Math'
+        },
+        {
+          date: new Date(),
+          title: 'Cooking Emus',
+          synopsis: 'Extinction tastes divine',
+          user: user.id,
+          rating: '4',
+          category: 'Food'
+        },
+        {
+          date: new Date(),
+          title: 'How to touch the stars',
+          synopsis: 'Reach for greatness and incinerate yourself',
+          user: user.id,
+          rating: '3',
+          category: 'The Universe'
+        },
+        {
+          date: new Date(),
+          title: 'Awesome Plan',
+          synopsis: 'a plan for world domination',
+          user: user.id,
+          rating: '2',
+          category: 'life'
+        }
+      );
+
+      Plan.find({}).remove(function(){
+        Plan.create(plans, function(err, plan) {
+            console.log(err);
+            console.log('finished populating plans');
+          }
+        );
+      });
+    
+
+
     }
   );
 });
 
-var Plan = require('../api/plan/plan.model');
 
-Plan.find({}).remove(function(){
-  Plan.create({
-    date: new Date(),
-    title: 'Learn Javascript',
-    synopsis: 'join the world of programming',
-    user: 'test@test.com',
-    rating: '4',
-    category: 'Math'
-  }, {
-    date: new Date(),
-    title: 'Learn To Fly',
-    synopsis: 'flying is such an amazing expernce.',
-    user: 'test@test.com',
-    rating: '5',
-    category: 'other'
-  },{
-    date: new Date(),
-    title: 'Carl Seigan rules',
-    synopsis: 'Need I say more?',
-    user: 'test@test.com',
-    rating: '5',
-    category: 'The Universe'
-  },{
-    date: new Date(),
-    title: 'Learn to Cook',
-    synopsis: 'Be the life of the party by cooking.',
-    user: 'admin@admin.com',
-    rating: '3',
-    category: 'Food'
-  },{
-    date: new Date(),
-    title: 'Klingon',
-    synopsis: 'nerds',
-    user: 'test@test.com',
-    rating: '2',
-    category: 'Languages'
-  },{
-    date: new Date(),
-    title: 'Much Music Making',
-    synopsis: 'More bass in this place',
-    user: 'test@test.com',
-    rating: '4',
-    category: 'Music'
-  },{
-    date: new Date(),
-    title: 'Mathematics by Socrates',
-    synopsis: 'because 1 + 1 =  wisdom',
-    user: 'test@test.com',
-    rating: '1',
-    category: 'Math'
-  },{
-    date: new Date(),
-    title: 'Cooking Emus',
-    synopsis: 'Extinction tastes divine',
-    user: 'test@test.com',
-    rating: '4',
-    category: 'Food'
-  },{
-    date: new Date(),
-    title: 'How to touch the stars',
-    synopsis: 'Reach for greatness and incinerate yourself',
-    user: 'test@test.com',
-    rating: '3',
-    category: 'The Universe'
-  },{
-    date: new Date(),
-    title: 'Awesome Plan',
-    synopsis: 'a plan for world domination',
-    user: 'admin@admin.com',
-    rating: '2',
-    category: 'life'
-  }, function() {
-      console.log('finished populating plans');
-    }
-  );
-});
+
