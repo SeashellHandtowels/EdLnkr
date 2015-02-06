92415643af0f380750d24a1dd774c9bed7378014
Tyler Davis <datadazer@gmail.com>
2015-02-05 20:17:36 -0800

    updated frontpage. broken css.

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -1,25 +1,12 @@
 'use strict';
 
 angular.module('edLnkrApp')
-  .config(function ($stateProvider) {
-    $stateProvider
-      .state('main', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
-      });
-  });
-angular.module('ui.bootstrap.carousel').controller('CarouselDemoCtrl', function ($scope) {
-  $scope.myInterval = 10000;
-  var slides = $scope.slides = [];
-  $scope.addSlide = function() {
-    // var newWidth = 800 + slides.length + 1;
-    slides.push({
-      image: '../../assets/images/books.jpg' //+ newWidth
-      //text: ['Our app is cool!','Tons of features','Gorgeous Interface','Teach stuff'][slides.length % 4]
+.config(function ($stateProvider) {
+  $stateProvider
+    .state('main', {
+      url: '/',
+      templateUrl: 'app/main/main.html',
+      controller: 'MainCtrl',
+      authenticate: false
     });
-  };
-  for (var i=0; i<1; i++) {
-    $scope.addSlide();
-  }
 });
